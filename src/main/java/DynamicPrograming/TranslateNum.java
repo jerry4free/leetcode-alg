aeec6c48c2dec2a30e9b06774f5617d64ba2fe41
vancexie <vancexie@tencent.com>
2022-09-09 18:14:07 +0800

    feat: Add offer 46

---
+++ src/main/java/DynamicPrograming/TranslateNum.java
@@ -0,0 +1,54 @@
+package DynamicPrograming;
+
+import Util.Utils;
+
+/**
+ * offer 46
+ */
+public class TranslateNum {
+    // 注意点：如果知道重点考察的算法，其他不重要的可以快速简单实现，或调用系统API。
+    public int translateNum(int num) {
+        String s = String.valueOf(num); // 转化成string，简化数值从高到低的遍历
+
+        int len = s.length();
+        int[] dp = new int[len];
+
+        dp[0] = 1;
+
+        /**
+         *
+         * 状态转移方程：
+         * 1. dp[i] = dp[i-1] + dp[i-2], if 10 <= num[i-1]*10+num[i] <= 25
+         * 2. dp[i] = dp[i-1], if num[i-1]*10+num[i] > 25
+         * 注意边界case：00,01,...09这种不能组成2种翻译
+          */
+        for (int i = 1; i < len; i++){
+            int n = (s.charAt(i-1) - '0') * 10 + s.charAt(i) - '0';
+            if (i == 1){
+                if (n < 26 && n >= 10){
+                    dp[i] = 2;
+                } else {
+                    dp[i] = dp[i-1];
+                }
+            } else {
+                if (n < 26 && n >= 10){
+                    dp[i] = dp[i-1] + dp[i-2];
+                } else {
+                    dp[i] = dp[i-1];
+                }
+            }
+        }
+//        Utils.show(dp);
+
+        return dp[len-1];
+    }
+
+    public static void main(String[] args) {
+        TranslateNum inst = new TranslateNum();
+        System.out.println(inst.translateNum(0));
+        System.out.println(inst.translateNum(3));
+        System.out.println(inst.translateNum(25));
+        System.out.println(inst.translateNum(12258));
+
+    }
+}
---
+++ src/main/java/Util/Utils.java
@@ -0,0 +1,11 @@
+package Util;
+
+public class Utils {
+
+    public static void show(int[] n){
+        for (int i : n){
+            System.out.print(i + ", ");
+        }
+        System.out.println("");
+    }
+}
