fd051084b4fc6e8e13d526b95984bb642a5d11bb
jerry <yanbin.tse@gmail.com>
2022-09-28 00:34:45 +0800

    feat: Update 368 最大整除子集

--- src/main/java/DynamicPrograming/LargestDivisibleSubset.java
+++ src/main/java/DynamicPrograming/LargestDivisibleSubset.java
@@ -4,7 +4,6 @@ import Util.Utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -15,7 +14,8 @@ public class LargestDivisibleSubset {
         int n = nums.length;
         Arrays.sort(nums);
 
-        // 假设f(i)表示前i个正数的最大子集的个数, f1(i)表示
+        // 由于除法具有传递性，所以将nums升序排列后，可以找到某子集结尾数，就是该子集的最大整数, 不用再往前找了，这样就可以状态转移
+        // 假设f(i)表示前i个正数，且以nums[i]为最大整数为结尾的整除子集的长度
         // 那么对于nums[i]，
         // f(i) = max{ f(j) + 1, if nums[i-1] % nums[j] == 0},  1 <= j < i
         int[] f = new int[n];
@@ -27,9 +27,9 @@ public class LargestDivisibleSubset {
         }
 
         int maxLength = 1; // 最长子集的长度
-        int maxValue = nums[0];  // 最长子集的最大值
+        int maxValue = nums[0];  // 最长子集的最大值，用来倒推取子集需要
         for (int i = 1; i < n; i++){
-            for (int j = 0; j < i; j++){
+            for (int j = 0; j < i; j++){  // 可能存在多个子集，所以需要遍历所有i之前的状态
                 if (nums[i] % nums[j] == 0 ){
                     f[i] = Math.max(f[i], f[j] + 1);
                     if (f[i] > maxLength){
@@ -43,9 +43,9 @@ public class LargestDivisibleSubset {
 //        Utils.show(f1);
 
         List<Integer> ret = new ArrayList<>();
-        // maxValue就是最长子集中的最大倍数
+        // 倒推取最长子集
         for (int i = n-1; i >= 0; i--){
-            if (maxValue % nums[i] == 0 && f[i] == maxLength){ // 注意还需要判断i对应的状态：即子集长度
+            if (maxValue % nums[i] == 0 && f[i] == maxLength){ // 注意还需要判断i对应的状态：即子集长度，否则可能找到其他不是最长子集的元素上
                 ret.add(nums[i]);
                 maxValue = nums[i];  // 随着子集减小，更新子集的最大值
                 maxLength--;
