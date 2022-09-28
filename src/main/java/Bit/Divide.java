30143b35195253da0ca0fce9e8367585ed4f54c8
vancexie <vancexie@tencent.com>
2022-09-28 20:58:08 +0800

    feat: Add 50

--- src/main/java/Bit/Divide.java
+++ src/main/java/Bit/Divide.java
@@ -1,10 +1,14 @@
 package Bit;
 
+/**
+ * 29 两数相除
+ */
 public class Divide {
     public int divide(int dividend, int divisor) {
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
         }
+        // 因为int的范围是-2^31, 2^31-1, 所以-2^31转成正数会越界。所以反过来不会越界
         int tag = 2;
         if (dividend > 0) {
             dividend = -dividend;
@@ -14,10 +18,10 @@ public class Divide {
             divisor = -divisor;
             tag--;
         }
-//        Integer.MIN_VALUE;
 
         int ret = 0;
         while (dividend <= divisor){
+            // 成倍的去减，借鉴倍增的思想，用来加快减法
             int kd = divisor;
             int k = 1;
             while (kd > dividend - kd){  // 如果kd + kd > dividend, 采用加法，两个大数相加可能越界，采用减法不会
@@ -36,9 +40,24 @@ public class Divide {
         }
     }
 
+    // 用加法实现，快速乘
+    static long mul(long a, long k) {
+        long ans = 0;
+        while (k > 0) {
+            if ((k & 1) == 1) ans += a;
+//            System.out.println("ans:" + ans + ",k:" + k);
+            k >>= 1;
+            a += a;
+        }
+        return ans;
+    }
+
+
     public static void main(String[] args) {
         Divide inst = new Divide();
         System.out.println(inst.divide(15, 2));
         System.out.println(inst.divide(2147483647, 3)); //715827882
+
+        System.out.println(mul(101L, 5L));
     }
 }
