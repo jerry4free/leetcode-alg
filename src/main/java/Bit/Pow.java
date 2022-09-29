17fdbb03a3b8fdb0fb17a27ff617b5b1d3ab10ec
vancexie <vancexie@tencent.com>
2022-09-29 15:58:33 +0800

    fix: Fix int overflow issue

--- src/main/java/Bit/Pow.java
+++ src/main/java/Bit/Pow.java
@@ -9,19 +9,18 @@ public class Pow {
             return 1D;
         }
         int flag = (n < 0) ? -1: 1;
-        // -2147483648取反时，要注意避免越界
-        // Integer.MIN_VALUE取反，还是自身。调用abs也还是自身，本身避免了越界
-        n = Math.abs(n);
+        long N = n;
+        if (N < 0){
+            N = -N;
+        }
 
         // 采用相乘实现幂，倍增的思想来加快相乘
         double ret = 1.0;
-        while (n > 0){
-            if ((n & 1) == 1) ret *= x;
-            n >>= 1;
+        while (N > 0){
+            if ((N & 1) == 1) ret *= x;
+            N >>= 1;
             x *= x;
         }
-
-//        System.out.println(flag);
         return (flag < 0) ? (1/ret) : ret;
     }
 
