16a835e263b937d9abd606ab53b961e59c6ca411
vancexie <vancexie@tencent.com>
2022-10-14 11:22:08 +0800

    feat: Add 143. Reorder List

--- src/main/java/LinkedListX/ReorderList.java
+++ src/main/java/LinkedListX/ReorderList.java
@@ -1,12 +1,12 @@
 package LinkedListX;
 
 /**
- * 143
+ * 143. Reorder List
  */
 public class ReorderList {
     public void reorderList(ListNode head) {
         if (head == null || head.next == null) {
-            return head;
+            return;
         }
 
         ListNode curr = head;
@@ -27,7 +27,35 @@ public class ReorderList {
         prev.next = null;
 
         // reverse second
+        curr = second;
+        prev = null;
+        ListNode next;
+        while (curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        second = prev;
 
+        // merge two linked list
+        ListNode curr1 = head;
+        ListNode next1;
+        ListNode curr2 = second;
+        ListNode next2;
+        while (curr1 != null && curr2 != null){
+            next1 = curr1.next;
+            next2 = curr2.next;
+
+            curr1.next = curr2;
+
+            if (next1 != null){
+                curr2.next = next1;
+            }
+
+            curr2 = next2;
+            curr1 = next1;
+        }
     }
 
     public static void main(String[] args) {
