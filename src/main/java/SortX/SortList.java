7814f6c1f68339a2a6d3aa4a79135e107a62093e
vancexie <vancexie@tencent.com>
2022-10-17 20:19:02 +0800

    feat: Update 148. Sort List

--- src/main/java/SortX/SortList.java
+++ src/main/java/SortX/SortList.java
@@ -33,10 +33,46 @@ public class SortList {
         return dummy.next;
     }
 
+    private ListNode split(ListNode head){
+        ListNode dummy = new ListNode(0, head);
+        // 加上dummy后，避免只有2个节点时，无法截断.
+        // 循环完，slow.next是后半段的开始
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode head2 = slow.next;
+        slow.next = null;
+
+        return head2;
+    }
+
     /**
-     * bottom-up
+     * top-down
      */
     public ListNode sortList(ListNode head) {
+        if (head == null){
+            return null;
+        }
+        if (head.next == null){
+            return head;
+        }
+
+        ListNode head2 = split(head);
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(head2);
+
+        return merge(left, right);
+    }
+
+    /**
+     * bottom-up
+     * TODO: not finish
+     */
+    public ListNode sortList2(ListNode head) {
         int len = 0;
         ListNode curr = head;
         while (curr != null){
@@ -44,18 +80,44 @@ public class SortList {
             curr = curr.next;
         }
 
-        ListNode fast;
-        ListNode slow;
-        ListNode prev;
+//        ListNode dummy = new ListNode(0, head);
+        ListNode next;
+        ListNode head1;
+        ListNode head2;
         for (int sz = 1; sz < len; sz *= 2){
-            fast = head;
-            slow = head;
-            while (prev != null && prev.next != null){
-                prev = merge(dummy, sz);
+            curr = head;
+            while (curr != null){
+                head1 = curr;
+
+                // 获取head2，
+                for (int i = 1; i < sz && curr.next != null; i++){
+                    curr = curr.next;
+                }
+                head2 = curr.next;
+                curr.next = null;
+
+                if (head2 == null){ // 长度不足sz，没有head2
+                    break;
+                }
+
+                // 获取下一次的next，并截断
+                for (int i = 1; i < sz && curr != null; i++){
+                    curr = curr.next;
+                }
+                if (curr != null && curr.next != null){
+                    next = curr.next;
+                    curr.next = null;
+                    curr = next;
+                }
+
+                // if head1 and head2, then merge
+                merge(head1, head2);
             }
+
+
         }
 
-        return dummy.next;
+        return head;
     }
 
     public static void main(String[] args) {
