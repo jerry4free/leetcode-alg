953426344923f189b080c9e4491033c31c8c8082
vancexie <vancexie@tencent.com>
2022-10-20 20:54:01 +0800

    feat: Add 84

---
+++ src/main/java/StackX/LargestRectangleArea.java
@@ -0,0 +1,14 @@
+package StackX;
+
+import java.util.Stack;
+
+/**
+ * 84.
+ */
+public class LargestRectangleArea {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+
+    }
+
+}
