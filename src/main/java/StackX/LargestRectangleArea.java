3d705d83eba12ec3792e90789918c6e58c33ed75
vancexie <vancexie@tencent.com>
2022-10-21 21:04:49 +0800

    feat: Add 84.柱状图中最大的矩形

--- src/main/java/StackX/LargestRectangleArea.java
+++ src/main/java/StackX/LargestRectangleArea.java
@@ -3,12 +3,41 @@ package StackX;
 import java.util.Stack;
 
 /**
- * 84.
+ * 84.柱状图中最大的矩形
  */
 public class LargestRectangleArea {
+    /**
+     * 单调栈中存储下标，方便计算宽度
+     * 时间和空间复杂度都是O(N)
+     */
     public int largestRectangleArea(int[] heights) {
+        // 栈中存储的是单调递增的高度，代表尚未确定下一次更低的高度
         Stack<Integer> stack = new Stack<>();
+        // base case：存储-1下标, 代表柱子最左侧的边界（不包含）
+        stack.push(-1);
 
+        int ret = 0;
+        int len = heights.length;
+        for (int i = 0; i < len; i++){
+            // 如果第i个柱子小，那么遇到了更低的高度，则可以算之前的累积的高柱子了。
+            while(stack.peek() != -1 && heights[stack.peek()] > heights[i]) {
+                int height = heights[stack.pop()];
+                int width = i - stack.peek() - 1;  // i是柱子的右边界，stack.peek()是柱子的左边界，左开右开
+                int area = height * width;
+                ret = Math.max(ret, area);
+            }
+            stack.push(i);
+        }
+
+        // 最后栈中累积的递增柱子
+        while(stack.peek() != -1){
+            int height = heights[stack.pop()];
+            int width = len - stack.peek() - 1;
+            int area = height * width;
+            ret = Math.max(ret, area);
+        }
+
+        return ret;
     }
 
 }
