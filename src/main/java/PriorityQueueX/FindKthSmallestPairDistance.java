fe32829ec6d0ba89080b4257903e2da9c2209c9c
jerry <yanbin.tse@gmail.com>
2022-12-26 23:15:01 +0800

    feat: Update 719. Find K-th Smallest Pair Distance

--- src/main/java/PriorityQueueX/FindKthSmallestPairDistance.java
+++ src/main/java/PriorityQueueX/FindKthSmallestPairDistance.java
@@ -10,11 +10,42 @@ import java.util.*;
 public class FindKthSmallestPairDistance {
 
     /**
+     * 二分查找
+     */
+    public int smallestDistancePair(int[] nums, int k) {
+        int len = nums.length;
+        Arrays.sort(nums);
+
+        // 由于最大距离是dist，所以要找的第k小的数对距离一定是在[0,dist]中间
+        int dist = nums[len - 1] - nums[0];
+        int lo = 0;
+        int hi = dist;
+        while (lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            int smallCnt = getSmallCnt(nums, mid);
+            if (smallCnt < k){
+                lo = mid + 1;
+            } else if (smallCnt == k) {  // 相等
+                hi = mid - 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    // 计算所有小于等于d距离的数对个数
+    private int getSmallCnt(int[] nums, int d){
+        return 1;
+    }
+
+    /**
      * 本方法not AC，当k很大时时间超限，有个测试用例的k是25000000！！！
      * 时间复杂度：O(max(N,k) * lgN)
      * 空间复杂度：O(N)
      */
-    public int smallestDistancePair(int[] nums, int k) {
+    public int smallestDistancePair2(int[] nums, int k) {
         // 多路合并的思想.
         // reduction: 由于每一路都有序,可以通过最小堆来进行合并
         int n = nums.length;
