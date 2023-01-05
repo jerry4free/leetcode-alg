65a627d87523ad68d55c747d3b86fd17f686506b
vancexie <vancexie@tencent.com>
2023-01-05 20:54:53 +0800

    feat: Update 234

--- src/main/java/LinkedListX/PalindromeLinkedList.java
+++ src/main/java/LinkedListX/PalindromeLinkedList.java
@@ -13,7 +13,7 @@ public class PalindromeLinkedList {
      */
     ListNode second;
     boolean isValid;
-    public boolean isPalindrome(ListNode head) {
+    public boolean isPalindrome2(ListNode head) {
         if (head == null || head.next == null){
             return true;
         }
@@ -51,6 +51,71 @@ public class PalindromeLinkedList {
             isValid = false;
         }
         second = second.next;
+    }
+
+    private ListNode secondHead;
+    public boolean isPalindrome(ListNode head) {
+
+        // 0或1个节点
+        if (head == null || head.next == null){
+            return true;
+        }
+
+        ListNode dummy = new ListNode(0, head);
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        // 通过快慢指针，找到中间节点
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+
+        ListNode second = slow.next;
+        slow.next = null;
+        // 对第2部分链表进行倒序
+        ListNode secondTail = reverse(second);
+        secondTail.next = null;  // 对翻转后的链表截断
+        ListNode curr2 = secondHead;
+        // 对第一部分结尾截断
+        ListNode curr1 = head;
+
+        show(curr1);
+        show(curr2);
+
+        // 双指针进行遍历判断
+        while (curr2 != null){
+            if (curr2.val != curr1.val){
+                return false;
+            }
+            curr2 = curr2.next;
+            curr1 = curr1.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverse(ListNode head){
+        if (head.next == null){
+            secondHead = head;
+            return secondHead;
+        }
+
+        // 递归式的翻转后面的链表
+        ListNode next = reverse(head.next);
+        next.next = head;
+
+        return head;
+    }
+
+    private void show(ListNode head){
+
+        while (head != null){
+            System.out.print("->" + head.val);
+            head = head.next;
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
