13dec917d2f6a45213a318c24124f2719a370d46
yanbin.tse <yanbin.tse@gmail.com>
2024-07-29 10:17:51 +0800

    feat:Update UglyNumberII

--- src/main/java/PriorityQueueX/UglyNumberII.java
+++ src/main/java/PriorityQueueX/UglyNumberII.java
@@ -7,6 +7,7 @@ import java.util.*;
  */
 public class UglyNumberII {
 
+    // 时间复杂度O(NlgN),空间复杂度O(N)
     final public static int[] primes = new int[]{2,3,5};
     public int nthUglyNumber(int n) {
         PriorityQueue<Long> q = new PriorityQueue<>();
@@ -16,18 +17,17 @@ public class UglyNumberII {
 
         long ret = 0;
         for (int i = 0; i < n; i++){
-            ret = q.remove();
-            q.remove(ret);
+            ret = q.remove();  // O(1)
             for (int j = 0; j < 3; j++){
-                Long next = primes[j] * ret;
+                long next = primes[j] * ret;
                 if (!s.contains(next)){
-                    q.add(next);
+                    q.add(next); // O(lgN)
                     s.add(next);
                 }
             }
         }
 
-        return (int)(ret);
+        return (int)ret;
     }
 
     public static void main(String[] args) {
