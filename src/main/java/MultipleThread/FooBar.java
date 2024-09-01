a2da6bccf8a476153e655fba15eee463edf71408
jerry <yanbin.tse@gmail.com>
2024-09-01 18:10:37 +0800

    feat: Update FooBar

--- src/main/java/MultipleThread/FooBar.java
+++ src/main/java/MultipleThread/FooBar.java
@@ -1,28 +1,70 @@
 package MultipleThread;
 
 import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * 1115. 交替打印 FooBar
+ */
 public class FooBar {
 
     private int n;
-    // 当前有1个许可证，不是说容量只能有1个许可证。release()N次后，可以有超过N个的许可证
+    // 方法1：Semaphore
+    // 注意：当前有1个许可证，不是说容量只能有1个许可证。release()N次后，可以有超过N个的许可证
     final Semaphore foo = new Semaphore(1);
     final Semaphore bar = new Semaphore(0);
 
+    // 方法2：阻塞队列
+    // 队列为空时，take会阻塞。
     private BlockingDeque<Integer> fooQueue = new LinkedBlockingDeque<>();
     private BlockingDeque<Integer> barQueue = new LinkedBlockingDeque<>();
     // AtomicBoolean不行，因为不会阻塞
-//    AtomicBoolean flag = new AtomicBoolean(false);
+    // CountDownLatch不行，因为是计数只能使用一次。
+
+    // 方法3：ReentrantLock + volatile变量，本质就是公平锁和一个共享变量作为开关
+    private ReentrantLock lock = new ReentrantLock(true); // 公平锁
+    private volatile boolean fooExec = true;
 
     public FooBar(int n) {
         this.n = n;
         fooQueue.add(0);
     }
 
-    public void foo(Runnable printFoo) throws InterruptedException {
+    public void foo3(Runnable printFoo) throws InterruptedException {
+        for (int i = 0; i < n;) {
+            try {
+                lock.lock();
+                if (fooExec) {
+                    printFoo.run();
+                    i++;
+                    fooExec = false;
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public void bar3(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n;) {
+            try {
+                lock.lock();
+                if (!fooExec) {
+                    printBar.run();
+                    i++;
+                    fooExec = true;
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public void foo2(Runnable printFoo) throws InterruptedException {
 
         for (int i = 0; i < n; i++) {
             foo.acquire(); // may block
@@ -32,7 +74,7 @@ public class FooBar {
         }
     }
 
-    public void bar(Runnable printBar) throws InterruptedException {
+    public void bar2(Runnable printBar) throws InterruptedException {
 
         for (int i = 0; i < n; i++) {
             bar.acquire(); // bar许可证-1，may block
@@ -78,21 +120,5 @@ public class FooBar {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-//        AtomicBoolean ff = new AtomicBoolean(true);
-//        System.out.println(ff);
-
-        /*
-        PrintBar printBar = new PrintBar();
-        PrintFoo printFoo = new PrintFoo();
-
-        FooBar fooBar = new FooBar(3);
-        try {
-            fooBar.foo(printFoo);
-            fooBar.bar(printBar);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-         */
     }
 }
